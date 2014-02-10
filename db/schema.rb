e3633babcb47a278f291c14835969780b0793b83
Calvin Lam <calvinwlam@gmail.com>
2014-02-09 21:17:34 -0800

    trying to fix files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207093236) do
+ActiveRecord::Schema.define(version: 20140206202210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,8 @@ ActiveRecord::Schema.define(version: 20140207093236) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree

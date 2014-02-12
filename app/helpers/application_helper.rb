37c93bbef5d8aca5b229a21acf519c2041abee51
Calvin Lam <calvinwlam@gmail.com>
2014-02-12 15:42:12 -0800

    still trying to fix s3 connection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-
-  AWS::S3::Base.establish_connection!(
-    :access_key_id     =>  ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET'])
-  def download_url_for(file)
-    @file = 'app/assets/audios/recipe_id.mp3'
-    AWS::S3::S3Object.url_for(@file, 'tennis-testing', :authenticated => false)
-  end
 end

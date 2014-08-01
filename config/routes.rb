02bc62e08d4d36557ee7b0affc69f58e451724d6
Calvin Lam <calvinwlam@gmail.com>
2014-08-01 15:21:31 -0700

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Firstapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'site#index'
+  root 'site_recipes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

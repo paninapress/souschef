882573db115679893a93c403e20096166222336e
Calvin Lam <calvinwlam@gmail.com>
2014-08-09 12:15:54 -0700

    star rating broke search

--- app/controllers/site_recipes_controller.rb
+++ app/controllers/site_recipes_controller.rb
@@ -207,6 +207,8 @@ if box_nutrition[6].nil?
     end
 
     @recipes = SiteRecipe.last(12)
+    @rating = Rating.where(site_recipe_id: @recipes.id, user_id: @current_user).first unless @rating 
+@rating = Rating.create(site_recipe_id: @recipes.id, user_id: @current_user, score: 0)
   end
 
 
